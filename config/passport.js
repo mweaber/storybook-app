b93492d39cacafd0e9f755255186840ab8132e7d
Matthew Weaber <matthewweaber89@gmail.com>
2019-07-13 18:24:06 -0400

    Set up logs on route

--- config/passport.js
+++ config/passport.js
@@ -14,7 +14,7 @@ module.exports = function(passport){
             callbackURL: '/auth/google/callback',
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
-            // console.log(accessToken);
+            console.log(accessToken);
             // console.log(profile);
             const userImage = profile.photos[0].value;
             const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
@@ -40,7 +40,8 @@ module.exports = function(passport){
                     // Create User
                     new User(newUser)
                         .save()
-                        .then(user => done(null, user));
+                        .then(user => done(null, user))
+                        .catch(err => console.log(err));
                 }
             })
         })
