6ede6670db72c7368792985b3a8d3031b5d8d264
Matthew Weaber <matthewweaber89@gmail.com>
2019-07-17 18:51:22 -0400

    Forget to add keys

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ module.exports = function(passport){
         new GoogleStrategy({
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: absoluteURI + '/auth/google/callback',
+            callbackURL: keys.absoluteURI + '/auth/google/callback',
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
             // console.log(accessToken);
