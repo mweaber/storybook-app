15da4fc288f8aeec5c9de381719fa475b4e90043
Matthew Weaber <matthewweaber89@gmail.com>
2019-07-22 18:31:40 -0400

    logs and debugging

--- config/passport.js
+++ config/passport.js
@@ -15,9 +15,10 @@ module.exports = function(passport){
             callbackURL:'/auth/google/callback',
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
-            // console.log(accessToken);
+            console.log(accessToken);
+            console.log(refreshToken);
             console.log(callbackURL);
-            // console.log(profile);
+            console.log(profile);
             console.log(callbackURL);
             const userImage = profile.photos[0].value;
             const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
